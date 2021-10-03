dcca39e29300b4c85bdcdbae9d25f572b0bcdb44
antoine <antoine.doeraene@b12-consulting.com>
2021-10-03 22:17:35 +0200

    documented wow game state provider for me in the future

--- server/src/main/scala/be/doeraene/data/WOWGameStateProvider.scala
+++ server/src/main/scala/be/doeraene/data/WOWGameStateProvider.scala
@@ -22,12 +22,27 @@ import org.slf4j.{Logger, LoggerFactory}
 import java.awt.image.BufferedImage
 import scala.util.Try
 
+/** The [[WOWGameStateProvider]] is responsible for decoding the game state from the Lua addon in WoW.
+  *
+  * The information is composed of three different parts:
+  *   - One line with three squares (black - white - black) used to calibrate (infer) the size in pixels of the squares
+  *   - One encoding the number of squares that are used afterwards (there are 4 squares which encodes this information)
+  *   - A bunch of lines with all the squares that actually encode the game state.
+  *
+  * The game state is first encoded in JSON data, then this JSON is encoded as a base 64 string. The base 64 strings is
+  * mapped to numbers from 0 to 63, which are then encoded in base 8 (two digits needed). In an RGB square, each channel
+  * is used to encode precisely one of these 8 digits (hence, two squares encode 3 64-characters.
+  *
+  * @param topLeft
+  */
 final class WOWGameStateProvider(topLeft: (Int, Int)) extends Provider[Either[Throwable, GameState]] {
 
   val logger: Logger = LoggerFactory.getLogger(getClass)
 
   extension (x: Long) def pow(exp: Int): Long = if exp == 0 then 1L else x * (x pow (exp - 1))
 
+  /** Maps a list of channel values (red, then green, then blue) to base 64 digits (from 0 to 63).
+    */
   def decodeByteGroupings(bytes: List[Int]): List[Long] =
     bytes
       .grouped(numberOfSquaresPerByte)
@@ -41,6 +56,7 @@ final class WOWGameStateProvider(topLeft: (Int, Int)) extends Provider[Either[Th
       )
       .toList
 
+  /** Finds the size of a square in pixel. */
   def inferSquarePixelSize(capture: BufferedImage): Int = {
     val line = (0 until 100)
       .map(xOffset => (topLeft._1 + xOffset, topLeft._2))
@@ -49,11 +65,12 @@ final class WOWGameStateProvider(topLeft: (Int, Int)) extends Provider[Either[Th
       .toList
 
     val startWhiteSquare = line.indexWhere(_ > 128) // finding first light pixel
-    val endLastSquare = line.indexWhere(_ < 128, startWhiteSquare)
+    val endWhiteSquare = line.indexWhere(_ < 128, startWhiteSquare) // finding first dark after first light
 
-    endLastSquare - startWhiteSquare
+    endWhiteSquare - startWhiteSquare
   }
 
+  /** Finds how many squares are used for encoding the game state. */
   def readWOWNumberOfBytes(capture: BufferedImage, squaresPixelSize: Int): Long = {
     val y = topLeft._2 + squaresPixelSize
     val numberOfBytesPositions = (0 until numberOfNumberOfBytesSquares * numberOfSquaresPerByte)
@@ -73,6 +90,7 @@ final class WOWGameStateProvider(topLeft: (Int, Int)) extends Provider[Either[Th
     ).reverse.zipWithIndex.map((byte, exp) => byte * (64 pow exp)).sum
   }
 
+  /** Decodes the JSON game state from the colour squares. */
   def readWOWBytes(numberOfBytes: Long, squaresPixelSize: Int, capture: BufferedImage) = {
     val rowSize = sqrtOfNumberOfDataSquares * numberOfSquaresPerByte
     val bytesPositions = (0 until (rowSize * sqrtOfNumberOfDataSquares))
